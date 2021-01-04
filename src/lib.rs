a75225f20291809098cd5818e097509650e38db7
narumincho <narumincho.starfy@gmail.com>
2021-01-04 09:27:42 +0900

    try wasm-pack

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,74 @@
 pub mod data;
 pub mod to_string;
 pub mod util;
+use wasm_bindgen;
+
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn code(name: &str) -> String {
+  let sample = data::Code {
+    export_definition_list: vec![data::ExportDefinition::Function(data::Function {
+      name: data::identifer::from_string(name),
+      document: String::from("ミドルウェア"),
+      type_parameter_list: vec![],
+      parameter_list: vec![
+        data::ParameterWithDocument {
+          name: data::identifer::from_string("request"),
+          document: String::from("リクエスト"),
+          r#type: data::Type::ImportedType(data::ImportedType {
+            module_name: String::from("express"),
+            name: data::identifer::from_string("Request"),
+          }),
+        },
+        data::ParameterWithDocument {
+          name: data::identifer::from_string("response"),
+          document: String::from("レスポンス"),
+          r#type: data::Type::ImportedType(data::ImportedType {
+            module_name: String::from("express"),
+            name: data::identifer::from_string("Response"),
+          }),
+        },
+      ],
+      return_type: data::Type::Void,
+      statement_list: vec![
+        data::Statement::VariableDefinition(data::VariableDefinitionStatement {
+          name: data::identifer::from_string("accept"),
+          r#type: data::Type::Union(Box::new(vec![data::Type::String, data::Type::Undefined])),
+          is_const: true,
+          expr: util::get(
+            util::get(
+              data::Expr::Variable(data::identifer::from_string("request")),
+              "headers",
+            ),
+            "accept",
+          ),
+        }),
+        data::Statement::If(data::IfStatement {
+          condition: (util::logical_and(
+            util::not_equal(
+              data::Expr::Variable(data::identifer::from_string("accept")),
+              data::Expr::UndefinedLiteral,
+            ),
+            util::call_method(
+              data::Expr::Variable(data::identifer::from_string("accept")),
+              "includes",
+              vec![data::Expr::StringLiteral(String::from("text/html"))],
+            ),
+          )),
+          then_statement_list: vec![data::Statement::EvaluateExpr(util::call_method(
+            data::Expr::Variable(data::identifer::from_string("response")),
+            "setHeader",
+            vec![
+              data::Expr::StringLiteral(String::from("content-type")),
+              data::Expr::StringLiteral(String::from("text/html")),
+            ],
+          ))],
+        }),
+      ],
+    })],
+    statement_list: vec![],
+  };
+  to_string::to_string(&sample, &data::CodeType::TypeScript)
+}
 
 #[test]
 fn test_empty() {
