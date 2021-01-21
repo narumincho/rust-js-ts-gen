19ac849a86af357070eee8ddb354a72d03a04333
narumincho <narumincho.starfy@gmail.com>
2021-01-22 08:49:52 +0900

    fix comment style

--- src/data.rs
+++ src/data.rs
@@ -6,6 +6,7 @@ pub mod identifer;
 pub enum CodeType {
     /// JavaScript. ブラウザで動作するプログラミング言語
     JavaScript,
+
     /// TypeScript. JavaScript に 型をつけた言語
     TypeScript,
 }
@@ -15,6 +16,7 @@ pub enum CodeType {
 pub struct Code {
     /// 外部に公開する定義
     pub export_definition_list: Vec<ExportDefinition>,
+
     /// 定義した後に実行するコード
     pub statement_list: Vec<Statement>,
 }
@@ -24,8 +26,10 @@ pub struct Code {
 pub enum ExportDefinition {
     /// 型定義. JavaScript では出力されない
     TypeAlias(TypeAlias),
+
     /// 関数
     Function(Function),
+
     /// 変数
     Variable(Variable),
 }
@@ -44,89 +48,64 @@ pub struct TypeAlias {
 
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct Function {
-    /**
-     * 外部に公開する関数の名前
-     */
+    /// 外部に公開する関数の名前
     pub name: identifer::Identifer,
-    /**
-     * ドキュメント
-     */
+
+    /// ドキュメント
     pub document: String,
-    /**
-     * 型パラメーターのリスト
-     */
+
+    /// 型パラメーターのリスト
     pub type_parameter_list: Vec<identifer::Identifer>,
-    /**
-     * パラメーター
-     */
+
+    /// パラメーター
     pub parameter_list: Vec<ParameterWithDocument>,
-    /**
-     * 戻り値の型
-     */
+
+    /// 戻り値の型
     pub return_type: Type,
-    /**
-     * 関数の本体
-     */
+
+    /// 関数の本体
     pub statement_list: Vec<Statement>,
 }
-/**
- * ドキュメント付きの関数のパラメーター. パラメーター名, ドキュメント, 型
- */
+/// ドキュメント付きの関数のパラメーター. パラメーター名, ドキュメント, 型
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct ParameterWithDocument {
-    /**
-     * パラメーター名
-     */
+    /// パラメーター名
     pub name: identifer::Identifer,
-    /**
-     * ドキュメント
-     */
+
+    /// ドキュメント
     pub document: String,
-    /**
-     * パラメーターの型
-     */
+
+    /// パラメーターの型
     pub r#type: Type,
 }
 
-/**
- * 関数のパラメーター. パラメーター名, ドキュメント
- */
+/// 関数のパラメーター. パラメーター名, ドキュメント
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct Parameter {
-    /**
-     * パラメーター名
-     */
+    /// パラメーター名
     pub name: identifer::Identifer,
-    /**
-     * パラメーターの型
-     */
+
+    /// パラメーターの型
     pub r#type: Type,
 }
 
 /// 変数
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct Variable {
-    /**
-     * 変数の名前
-     */
+    /// 変数の名前
     pub name: identifer::Identifer,
-    /**
-     * ドキュメント
-     */
+
+    /// ドキュメント
     pub document: String,
-    /**
-     * 変数の型
-     */
+
+    /// 変数の型
     pub r#type: Type,
-    /**
-     * 変数の式
-     */
+
+    /// 変数の式
     pub expr: Expr,
 }
 
-/**
- * 単項演算子
- */
+/// 単項演算子
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub enum UnaryOperator {
     Minus,
@@ -134,9 +113,7 @@ pub enum UnaryOperator {
     LogicalNot,
 }
 
-/**
- * 2項演算子
- */
+/// 2項演算子
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub enum BinaryOperator {
     Exponentiation,
@@ -159,9 +136,7 @@ pub enum BinaryOperator {
     LogicalOr,
 }
 
-/**
- * 式
- */
+/// 式
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub enum Expr {
     NumberLiteral(i32),
@@ -202,9 +177,7 @@ pub enum Statement {
     Switch(SwitchStatement),
 }
 
-/**
- * 型
- */
+/// 型
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub enum Type {
     Number,
@@ -224,408 +197,278 @@ pub enum Type {
     ScopeInGlobal(identifer::Identifer),
     StringLiteral(String),
 }
-/**
- * 単項演算子と適用される式
- */
+/// 単項演算子と適用される式
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct UnaryOperatorExpr {
-    /**
-     * 単項演算子
-     */
+    /// 単項演算子
     pub operator: UnaryOperator,
-    /**
-     * 適用される式
-     */
+
+    /// 適用される式
     pub expr: Expr,
 }
 
-/**
- * 2項演算子と左右の式
- */
+/// 2項演算子と左右の式
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct BinaryOperatorExpr {
-    /**
-     * 2項演算子
-     */
+    /// 2項演算子
     pub operator: BinaryOperator,
-    /**
-     * 左の式
-     */
+
+    /// 左の式
     pub left: Expr,
-    /**
-     * 右の式
-     */
+
+    /// 右の式
     pub right: Expr,
 }
-/**
- * 条件演算子
- */
+/// 条件演算子
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct ConditionalOperatorExpr {
-    /**
-     * 条件の式
-     */
+    /// 条件の式
     pub condition: Expr,
-    /**
-     * 条件がtrueのときに評価される式
-     */
+
+    /// 条件がtrueのときに評価される式
     pub then_expr: Expr,
-    /**
-     * 条件がfalseのときに評価される式
-     */
+
+    /// 条件がfalseのときに評価される式
     pub else_expr: Expr,
 }
-/**
- * 配列リテラルの要素
- */
+/// 配列リテラルの要素
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct ArrayItem {
-    /**
-     * 式
-     */
+    /// 式
     pub expr: Expr,
-    /**
-     * スプレッド ...a のようにするか
-     */
+
+    /// スプレッド ...a のようにするか
     pub spread: bool,
 }
-/**
- * オブジェクトリテラルの要素
- */
+/// オブジェクトリテラルの要素
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub enum Member {
     Spread(Expr),
     KeyValue(KeyValue),
 }
 
-/**
- * 文字列のkeyと式のvalue
- */
+/// 文字列のkeyと式のvalue
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct KeyValue {
-    /**
-     * key
-     */
+    /// key
     pub key: String,
-    /**
-     * value
-     */
+
+    /// value
     pub value: Expr,
 }
 
-/**
- * ラムダ式
- */
+/// ラムダ式
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct LambdaExpr {
-    /**
-     * パラメーターのリスト
-     */
+    /// パラメーターのリスト
     pub parameter_list: Vec<Parameter>,
-    /**
-     * 型パラメーターのリスト
-     */
+
+    /// 型パラメーターのリスト
     pub type_parameter_list: Vec<identifer::Identifer>,
-    /**
-     * 戻り値の型
-     */
+
+    /// 戻り値の型
     pub return_type: Type,
-    /**
-     * ラムダ式本体
-     */
+
+    /// ラムダ式本体
     pub statement_list: Vec<Statement>,
 }
-
-/**
- * インポートした変数
- */
+/// インポートした変数
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct ImportedVariable {
-    /**
-     * モジュール名, 使うときにはnamedインポートされ, そのモジュール識別子は自動的につけられる
-     */
+    /// モジュール名, 使うときにはnamedインポートされ, そのモジュール識別子は自動的につけられる
     pub module_name: String,
-    /**
-     * 変数名
-     */
+
+    /// 変数名
     pub name: identifer::Identifer,
 }
-/**
- * プロパティアクセス
- */
+/// プロパティアクセス
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct GetExpr {
-    /**
-     * 式
-     */
+    /// 式
     pub expr: Expr,
-    /**
-     * プロパティの式
-     */
+
+    /// プロパティの式
     pub property_expr: Expr,
 }
 
-/**
- * 式と呼ぶパラメーター
- */
+/// 式と呼ぶパラメーター
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct CallExpr {
-    /**
-     * 呼ばれる式
-     */
+    /// 呼ばれる式
     pub expr: Expr,
-    /**
-     * パラメーター
-     */
+    /// パラメーター
     pub parameter_list: Vec<Expr>,
 }
 
-/**
- * 型アサーション
- */
+/// 型アサーション
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct TypeAssertion {
-    /**
-     * 型アサーションを受ける式
-     */
+    /// 型アサーションを受ける式
     pub expr: Expr,
-    /**
-     * 型
-     */
+
+    /// 型
     pub r#type: Type,
 }
 
-/**
- * 代入文
- */
+/// 代入文
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct SetStatement {
-    /**
-     * 対象となる式. 指定の仕方によってはJSのSyntaxErrorになる
-     */
+    /// 対象となる式. 指定の仕方によってはJSのSyntaxErrorになる
     pub target: Expr,
-    /**
-     * 演算子を=の左につける
-     */
+
+    /// 演算子を=の左につける
     pub operator_maybe: Option<BinaryOperator>,
-    /**
-     * 式
-     */
+
+    /// 式
     pub expr: Expr,
 }
 
-/**
- * if文
- */
+/// if文
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct IfStatement {
-    /**
-     * 条件の式
-     */
+    /// 条件の式
     pub condition: Expr,
-    /**
-     * 条件がtrueのときに実行する文
-     */
+
+    /// 条件がtrueのときに実行する文
     pub then_statement_list: Vec<Statement>,
 }
 
-/**
- * ローカル変数定義
- */
+/// ローカル変数定義
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct VariableDefinitionStatement {
-    /**
-     * 変数名
-     */
+    /// 変数名
     pub name: identifer::Identifer,
-    /**
-     * 変数の型
-     */
+
+    /// 変数の型
     pub r#type: Type,
-    /**
-     * 式
-     */
+
+    /// 式
     pub expr: Expr,
-    /**
-     * constかどうか. falseはlet
-     */
+
+    /// constかどうか. falseはlet
     pub is_const: bool,
 }
 
-/**
- * ローカル関数定義
- */
+/// ローカル関数定義
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct FunctionDefinitionStatement {
-    /**
-     * 変数名
-     */
+    /// 変数名
     pub name: identifer::Identifer,
-    /**
-     * 型パラメーターのリスト
-     */
+
+    /// 型パラメーターのリスト
     pub type_parameter_list: Vec<identifer::Identifer>,
-    /**
-     * パラメーターのリスト
-     */
+
+    /// パラメーターのリスト
     pub parameter_list: Vec<ParameterWithDocument>,
-    /**
-     * 戻り値の型
-     */
+
+    /// 戻り値の型
     pub return_type: Type,
-    /**
-     * 関数本体
-     */
+
+    /// 関数本体
     pub statement_list: Vec<Statement>,
 }
 
-/**
- * for文
- */
+/// for文
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct ForStatement {
-    /**
-     * カウンタ変数名
-     */
+    /// カウンタ変数名
     pub counter_variable_name: identifer::Identifer,
-    /**
-     * ループの上限の式
-     */
+
+    /// ループの上限の式
     pub until_expr: Expr,
-    /**
-     * 繰り返す文
-     */
+
+    /// 繰り返す文
     pub statement_list: Vec<Statement>,
 }
 
-/**
- * forOf文
- */
+/// forOf文
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct ForOfStatement {
-    /**
-     * 要素の変数名
-     */
+    /// 要素の変数名
     pub element_variable_name: identifer::Identifer,
-    /**
-     * 繰り返す対象
-     */
+
+    /// 繰り返す対象
     pub iterable_expr: Expr,
-    /**
-     * 繰り返す文
-     */
+
+    /// 繰り返す文
     pub statement_list: Vec<Statement>,
 }
 
-/**
- * switch文
- */
+/// switch文
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct SwitchStatement {
-    /**
-     * switch(a) {} の a
-     */
+    /// switch(a) {} の a
     pub expr: Expr,
-    /**
-     * case "text": { statementList }
-     */
+
+    /// case "text": { statementList }
     pub pattern_list: Vec<Pattern>,
 }
 
-/**
- * switch文のcase "text": { statementList } の部分
- */
+/// switch文のcase "text": { statementList } の部分
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct Pattern {
-    /**
-     * case に使う文字列
-     */
+    /// case に使う文字列
     pub case_string: String,
-    /**
-     * statementList
-     */
+
+    /// statementList
     pub statement_list: Vec<Statement>,
 }
 
-/**
- * オブジェクトのメンバーの型
- */
+/// オブジェクトのメンバーの型
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct MemberType {
-    /**
-     * プロパティ名
-     */
+    /// プロパティ名
     pub name: String,
-    /**
-     * 必須かどうか falseの場合 ? がつく
-     */
+
+    /// 必須かどうか falseの場合 ? がつく
     pub required: bool,
-    /**
-     * 型
-     */
+
+    /// 型
     pub r#type: Type,
-    /**
-     * ドキュメント
-     */
+
+    /// ドキュメント
     pub document: String,
 }
 
-/**
- * 関数の型
- */
+/// 関数の型
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct FunctionType {
-    /**
-     * 型パラメーターのリスト
-     */
+    /// 型パラメーターのリスト
     pub type_parameter_list: Vec<identifer::Identifer>,
-    /**
-     * パラメーターの型. 意味のない引数名は適当に付く
-     */
+
+    /// パラメーターの型. 意味のない引数名は適当に付く
     pub parameter_list: Vec<Type>,
-    /**
-     * 戻り値の型
-     */
+
+    /// 戻り値の型
     pub return_type: Type,
 }
 
-/**
- * パラメーター付きの型
- */
+/// パラメーター付きの型
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct TypeWithTypeParameter {
-    /**
-     * パラメーターをつけられる型
-     */
+    /// パラメーターをつけられる型
     pub r#type: Type,
-    /**
-     * パラメーターに指定する型. なにも要素を入れなけければ T<>ではなく T の形式で出力される
-     */
+
+    /// パラメーターに指定する型. なにも要素を入れなけければ T<>ではなく T の形式で出力される
     pub type_parameter_list: Vec<Type>,
 }
 
-/**
- * 交差型
- */
+/// 交差型 `A & B`
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct IntersectionType {
-    /**
-     * 左に指定する型
-     */
+    /// 左に指定する型
     pub left: Type,
-    /**
-     * 右に指定する型
-     */
+
+    /// 右に指定する型
     pub right: Type,
 }
 
-/**
- * インポートされた型
- */
+/// インポートされた型
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct ImportedType {
     /// モジュール名 `fs` `three` `react` `typescript` `util` など
     pub module_name: String,
+
     /// 型の名前
     pub name: identifer::Identifer,
 }
